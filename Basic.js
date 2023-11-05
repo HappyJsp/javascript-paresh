1086308fbd481e386b306b1316920f529ee064d5
Prathamesh Bhavsar <149922966+HappyJsp@users.noreply.github.com>
2023-11-05 10:40:19 +0000

    Done

--- Basic.js
+++ Basic.js
@@ -1,2 +1,6 @@
+/* The code `console.log("hare krishna");` is printing the string "hare krishna" to the console.
+Similarly, `console.log("Welcome to learn JS With me ");` is printing the string "Welcome to learn
+JS With me" to the console. */
+
 console.log("hare krishna");
 console.log("Welcome to learn JS With me ");
---
+++ Variables.js
@@ -0,0 +1,24 @@
+
+/* The code is declaring a constant variable named `Fruit` and assigning it the value "Apple ". The
+`const` keyword is used to declare a variable that cannot be reassigned a new value once it has been
+initialized. In this case, the constant `Fruit` is assigned the string value "Apple ". 
+const Keyword Neither Be Update Neither be redecleare    */
+const Fruit = "Apple "
+console.log(Fruit);
+
+
+/* The code block enclosed in curly braces `{}` is creating a new scope. Within this scope, a variable
+named `Name` is declared using the `let` keyword and assigned the value `'paresh Bhavsar'`. The
+value of `Name` is then logged to the console using `console.log(Name)`.   Let : Block Scope */
+{
+
+    let Name = 'paresh Bhavsar' ;
+    console.log(Name);
+}
+
+/* The code is declaring a variable named `sirname` using the `var` keyword and assigning it the value
+"Juneja". The value of `sirname` is then logged to the console using `console.log(sirname)`. */
+
+var sirname = "Juneja"
+
+console.log(sirname);
