abcbdd5c9cdc1a0cfd6d4dc566cb4d2c6cc2f3db
Prathamesh Bhavsar <149922966+HappyJsp@users.noreply.github.com>
2023-11-05 11:55:49 +0000

    D

---
+++ Datatypes.js
@@ -0,0 +1,28 @@
+/*
+
+There are eight basic JavaScript data types:
+
+String: A sequence of characters enclosed in single or double quotes. For example, "Hello" or 'JavaScript'.
+Number: A numeric value, including both integers and decimals. For example, 10, 3.14, or -100.
+Bigint: An integer that can represent numbers larger than those that can be represented by the Number data type. For example, 12345678901234567890n.
+Boolean: A logical value that can be either true or false. For example, true or false.
+Undefined: A value that represents the absence of a value. For example, the value of a variable that has not been initialized is undefined.
+Null: A value that represents the intentional absence of a value. For example, a variable that is explicitly set to null indicates that it has no value.
+Symbol: A unique and immutable value that can be used as a property key. Symbols are often used as identifiers for private data or methods.
+Object: A collection of key-value pairs, where each key is a string and each value can be any JavaScript data type. Objects are used to store and organize data in JavaScript.
+*
+Data Type	Description	Example
+String	A sequence of characters	"Hello"
+Number	A numeric value	10, 3.14, -100
+Bigint	An integer that can represent numbers larger than those that can be represented by the Number data type	12345678901234567890n
+Boolean	A logical value that can be either true or false	true, false
+Undefined	A value that represents the absence of a value	undefined
+Null	A value that represents the intentional absence of a value	null
+Symbol	A unique and immutable value that can be used as a property key	Symbol("mySymbol")
+Object	A collection of key-value pairs	{ name: "John Doe", age: 30 }
+*/
+
+
+/*magine a shoebox filled with various items. Each item in the box has a label that helps you identify it. This box represents an object in JavaScript, and the items inside represent the object's properties. Each property has a name (like the label on the item) and a value (like the item itself).*/ 
+
+
